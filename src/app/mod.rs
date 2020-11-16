809c763a14bb5b718ca2bcc081e22e5bd30b3f22
MaximFischuk <mfischuk@vareger.com>
2020-11-16 13:21:51 +0200

    Add locking shared context macro

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -5,6 +5,7 @@ pub(crate) mod hooks;
 pub(crate) mod assert;
 pub(crate) mod job;
 
+use liquid::Object;
 use std::thread::sleep;
 use crate::app::executor::RunInfo;
 use crate::app::job::HttpJob;
@@ -23,9 +24,16 @@ use hyper::Body;
 use hyper::Client;
 use hyper::Request;
 use hyper_tls::HttpsConnector;
-use std::ops::DerefMut;
-use std::sync::Mutex;
-use std::iter::once;
+use std::sync::{Mutex, Arc};
+
+macro_rules! lock {
+    ($name: expr) => {
+        match $name.lock() {
+            Ok(locked) => locked,
+            Err(e) => panic!("{:#?}", e),
+        }
+    };
+}
 
 impl SendMessage<Request<Body>, ResponseFuture> for Client<HttpsConnector<HttpConnector>> {
     fn send(&self, data: Request<Body>) -> ResponseFuture {
@@ -37,7 +45,7 @@ pub struct App {
     name: String,
     jobs_group: JobGroup<HttpJob>,
     client: Client<HttpsConnector<HttpConnector>>,
-    context: Mutex<Context>,
+    context: Arc<Mutex<Context>>,
 }
 
 impl App {
@@ -55,7 +63,7 @@ impl App {
             client,
             name: manifest.name,
             jobs_group: JobGroup::new(String::default(), http_jobs),
-            context: Mutex::new(context),
+            context: Arc::new(Mutex::new(context)),
         }
     }
 
@@ -63,37 +71,36 @@ impl App {
         info!("Starting pipeline '{}'", self.name);
         info!("Registered {} jobs", self.jobs_group.amount());
         for (job, info) in self.jobs_group.iter() {
-            let mut lock = match self.context.lock() {
-                Ok(lock) => lock,
-                Err(e) => panic!("{:#?}", e),
-            };
-            let locked_context = lock.deref_mut();
-            let local_context = &mut locked_context.isolated(job.get_uuid());
+            let locked_context = lock!(self.context);
+            let mut local_context = locked_context.isolated(job.get_uuid());
+            drop(locked_context);
+            let mut exported = Object::default();
             for _ in 0..info.repeats {
                 sleep(info.delay);
                 // job.before(locked_context);
                 let now = Instant::now();
-                let result = job.execute(local_context, &self.client);
+                let result = job.execute(&mut local_context, &self.client);
                 info!("Elapsed for execution of test({}), {:?} ms", job.get_uuid(), now.elapsed().as_millis());
                 match result {
                     Ok(body) => {
                         for entry in &info.captures {
                             let mut assert_result: bool = true;
-                            let value = entry.cap.capture(local_context, &body);
+                            let value = entry.cap.capture(&mut local_context, &body);
                             for functor in &entry.on {
-                                assert_result &= functor.assert(local_context, &value);
+                                assert_result &= functor.assert(&mut local_context, &value);
                             }
                             info!("Captured value: {:?}", value);
                             if assert_result {
-                                locked_context.push_vars(once((entry.variable.clone().into(), value)));
+                                exported.insert(entry.variable.clone().into(), value);
                             }
                         }
                     },
                     Err(e) => error!("{:#?}", e)
                 }
             }
+            let mut locked_context = lock!(self.context);
+            locked_context.push_vars(exported);
             // job.after(locked_context);
-            drop(lock);
         }
     }
 }
